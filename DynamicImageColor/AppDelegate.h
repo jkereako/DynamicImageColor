bc3ed08ae0bcc802327b5b0753ab1d55c235277a
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-02-15 20:35:53 -0500

    Initial commit.

---
+++ DynamicImageColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicImageColor
+//
+//  Created by Jeffrey Kereakoglow on 2/14/15.
+//  Copyright (c) 2015 Alexis Digital. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicImageColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicImageColor
+//
+//  Created by Jeffrey Kereakoglow on 2/14/15.
+//  Copyright (c) 2015 Alexis Digital. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ViewController : UIViewController
+
+
+@end
+
